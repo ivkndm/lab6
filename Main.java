714ba34535bb0ad8c10fc98dfd92e88e8f07e2de
ivkndm <ivakindmi@gmail.com>
2024-10-20 21:30:29 +0300

    qwer

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ class Main{
         System.out.print("Это добавлено в ветке main");
         System.out.print("Это добавлено в ветке main 1");
         System.out.print("Это добавлено в ветке main 2");
+        System.out.print("Это добавлено в ветке main 3");
 
     }
 }
