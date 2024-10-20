ed32e9fbf0c30d70c9bb6ca8e4ea8c41aac584b1
ivkndm <ivakindmi@gmail.com>
2024-10-20 21:30:05 +0300

    qwer

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
-
+        System.out.print("Это добавлено в ветке main 1");
 
     }
 }
